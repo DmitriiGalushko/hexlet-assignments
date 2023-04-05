e6396a0f0eaa744d52abb5555a2450f993d1b4aa
@hexlet/cli <support@hexlet.io>
2023-04-06 00:38:39 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -7,8 +7,9 @@ import java.util.List;
 // BEGIN
 public class App {
     public static boolean scrabble(String charSet, String word) {
-        char[] charSetArr = charSet.toLowerCase().toCharArray();
-        char[] wordArr = word.toLowerCase().toCharArray();
+        var rightWord = word.toLowerCase();
+        char[] charSetArr = charSet.toCharArray();
+        char[] wordArr = rightWord.toCharArray();
         List<char[]> charSetList = new ArrayList<>(Arrays.asList(charSetArr));
         List<char[]> wordList = new ArrayList<>(Arrays.asList(wordArr));
 
