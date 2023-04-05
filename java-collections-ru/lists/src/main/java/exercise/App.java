ff56560a6603c1c589d606fb2be4eaf8e347f035
@hexlet/cli <support@hexlet.io>
2023-04-06 00:23:15 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,6 +1,7 @@
 package exercise;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // BEGIN
@@ -8,8 +9,8 @@ public class App {
     public static boolean scrabble(String charSet, String word) {
         char[] charSetArr = charSet.toLowerCase().toCharArray();
         char[] wordArr = word.toLowerCase().toCharArray();
-        List<char[]> charSetList = new ArrayList<>(List.of(charSetArr));
-        List<char[]> wordList = new ArrayList<>(List.of(wordArr));
+        List<char[]> charSetList = new ArrayList<>(Arrays.asList(charSetArr));
+        List<char[]> wordList = new ArrayList<>(Arrays.asList(wordArr));
 
         for (char[] ch : wordList) {
             if (charSetList.contains(ch)) {
